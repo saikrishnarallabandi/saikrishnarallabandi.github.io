a00c470fe8cbacc42381fd75325e5b2b4bb2d98b
saikrishnarallabandi <srallaba.research@gmail.com>
2025-12-10 14:53:10 -0600

    fixed paper title mismatch

--- js/publications.js
+++ js/publications.js
@@ -1,27 +1,19 @@
 const publications = [
     {
-        "title": "Speech synthesis using recurrent neural network trained on STRAIGHT extracted parameters",
-        "authors": "SK Rallabandi, G G G, SV Gangashetty",
-        "venue": "The 10th ISCA Speech Synthesis Workshop",
-        "year": "2013",
+        "title": "A survey of code-switched speech and language processing",
+        "authors": "S Sitaram, KR Chandu, SK Rallabandi, AW Black",
+        "venue": "arXiv preprint arXiv:1904.00784",
+        "year": "2019",
         "citations": 165,
         "url": "https://scholar.google.com/citations?view_op=view_citation&hl=en&user=7jXvu7MAAAAJ&citation_for_view=7jXvu7MAAAAJ:MXK_kJrjxJIC"
     },
     {
-        "title": "Unit selection based voice conversion using speaker space model",
-        "authors": "SK Rallabandi, SV Gangashetty",
-        "venue": "Twelfth Annual Conference of the International Speech Communication Association",
-        "year": "2011",
+        "title": "Experiments with Cross-lingual Systems for Synthesis of Code-Mixed Text.",
+        "authors": "S Sitaram, SK Rallabandi, S Rijhwani, AW Black",
+        "venue": "SSW, 76-81",
+        "year": "2016",
         "citations": 44,
         "url": "https://scholar.google.com/citations?view_op=view_citation&hl=en&user=7jXvu7MAAAAJ&citation_for_view=7jXvu7MAAAAJ:u-x6o8ySG0sC"
-    },
-    {
-        "title": "Identifying vocal cues in synthetic speech to reduce listening effort for users of cochlear implants",
-        "authors": "H Kim, M Lei, SK Rallabandi, B G, L H",
-        "venue": "Interspeech 2019",
-        "year": "2019",
-        "citations": 29,
-        "url": "https://scholar.google.com/citations?view_op=view_citation&hl=en&user=7jXvu7MAAAAJ&citation_for_view=7jXvu7MAAAAJ:IWHjjKOFINEC"
     },
     {
         "title": "Unsupervised self-training for sentiment analysis of code-switched data",
@@ -29,14 +21,22 @@ const publications = [
         "venue": "arXiv preprint arXiv:2103.14797",
         "year": "2021",
         "citations": 29,
+        "url": "https://scholar.google.com/citations?view_op=view_citation&hl=en&user=7jXvu7MAAAAJ&citation_for_view=7jXvu7MAAAAJ:IWHjjKOFINEC"
+    },
+    {
+        "title": "Speech Synthesis for Mixed-Language Navigation Instructions.",
+        "authors": "KR Chandu, SK Rallabandi, S Sitaram, AW Black",
+        "venue": "INTERSPEECH, 57-61",
+        "year": "2017",
+        "citations": 28,
         "url": "https://scholar.google.com/citations?view_op=view_citation&hl=en&user=7jXvu7MAAAAJ&citation_for_view=7jXvu7MAAAAJ:9yKSN-GCB0IC"
     },
     {
-        "title": "Error reduction network for dblstm based voice conversion",
-        "authors": "SK Rallabandi, G GG, SV Gangashetty",
-        "venue": "2015 IEEE International Conference on Acoustics, Speech and Signal Processing (ICASSP)",
-        "year": "2015",
-        "citations": 28,
+        "title": "On Building Mixed Lingual Speech Synthesis Systems.",
+        "authors": "SK Rallabandi, AW Black",
+        "venue": "Interspeech, 52-56",
+        "year": "2017",
+        "citations": 26,
         "url": "https://scholar.google.com/citations?view_op=view_citation&hl=en&user=7jXvu7MAAAAJ&citation_for_view=7jXvu7MAAAAJ:2osOgNQ5qMEC"
     }
 ];
